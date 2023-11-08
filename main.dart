26e62e63c15e4d84c7db2d757f38f59941e3f3c2
Samir-Sattarov <samirsattarov2004@gmail.com>
2023-11-08 17:51:28 +0500

    test commit

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void main() {
+  // print("Enter number ");
+
+  final List data = ["test", 3, 5, 6, 7, 8, 3, 1];
+
+  // for (var i = 0; i <= 9; i++) {
+  // print(i);
+  // }
+
+  // var i = 1;
+  // while (i < 1) {
+  // print(i);
+  // i++;
+  // }
+
+  // do {
+  // print(i);
+  // i++;
+  // } while (i <= 9);
+
+  // print("ForEach");
+  // data.forEach((element) {
+  // print(element);
+  // });
+
+  // print("For in");
+  // for (var element in data) {
+  // print(element);
+  // }
+}
